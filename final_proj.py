fa801550895195e0707e5d3459f2003d9dd90431
Abby Sypniewski <abbysyp@umich.edu>
2022-04-26 14:45:55 -0400

    limit how many items go into database at a time

--- final_proj.py
+++ final_proj.py
@@ -10,29 +10,30 @@ import csv
 import unittest
 import sqlite3
 
-def get_discog_songs(country):
+def get_discog_songs():
     """
     Return a list of dictionaries of the top 25 songs on Discogs for the given country using BeautifulSoup
     """
 
-    url = f'https://www.discogs.com/search/?sort=have%2Cdesc&ev=em_tr&format_exact=Single&country_exact={country}'
-    req = requests.get(url)
-    soup = BeautifulSoup(req.text, 'html.parser')
-    song_titles = soup.find_all('a', class_='search_result_title')
-    song_artists = soup.find_all('h5')
-
+    countries = ['US', 'France', 'Canada', 'UK']
     top_25_songs = []
 
-    for i in range(0, 25):
-        top_25_songs.append(
-            {
-                'ranking': i + 1,
-                'title': song_titles[i].text,
-                'artist': song_artists[i].find('a').text,
-                'country': country
-            }
-        )
+    for country in countries:
+        url = f'https://www.discogs.com/search/?sort=have%2Cdesc&ev=em_tr&format_exact=Single&country_exact={country}'
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, 'html.parser')
+        song_titles = soup.find_all('a', class_='search_result_title')
+        song_artists = soup.find_all('h5')
 
+        for i in range(0, 25):
+            top_25_songs.append(
+                {
+                    'ranking': i + 1,
+                    'title': song_titles[i].text,
+                    'artist': song_artists[i].find('a').text,
+                    'country': country
+                }
+            )
     return top_25_songs
 
 def setUpDatabase(db_name):
@@ -41,13 +42,23 @@ def setUpDatabase(db_name):
     cur = conn.cursor()
     return cur, conn
 
-def set_up_discogs_table(data, start, cur, conn):
+def set_up_discogs_table(data, cur, conn):
+    # cur.execute("DROP TABLE Discogs")
     cur.execute("CREATE TABLE IF NOT EXISTS Discogs (song_id INTEGER PRIMARY KEY, ranking INTEGER, country TEXT, title TEXT, artist TEXT)")
-    song_id = start
 
-    for song in data:
+    #select max id (last one put in db)
+    cur.execute('SELECT song_id FROM Discogs WHERE song_id  = (SELECT MAX(song_id) FROM Discogs)')
+    start = cur.fetchone()
+    if (start!= None):
+        start = start[0]
+    else:
+        start = 0
 
-        song_id = song_id + 1
+    id_counter = start
+    for song in data[start:start+25]:
+
+        id_counter = id_counter + 1
+        song_id = id_counter
         ranking = song['ranking']
         country = song['country']
         title = song['title']
@@ -60,12 +71,6 @@ def set_up_discogs_table(data, start, cur, conn):
 if __name__ == '__main__':
     cur, conn = setUpDatabase('music.db')
 
-    top_25_US_discogs = get_discog_songs('US')
-    top_25_FR_discogs = get_discog_songs('France')
-    top_25_CA_discogs = get_discog_songs('Canada')
-    top_25_UK_discogs = get_discog_songs('UK')
+    top_discogs = get_discog_songs()
 
-    set_up_discogs_table(top_25_US_discogs, 0, cur, conn)
-    set_up_discogs_table(top_25_FR_discogs, 25, cur, conn)
-    set_up_discogs_table(top_25_CA_discogs, 50, cur, conn)
-    set_up_discogs_table(top_25_UK_discogs, 75, cur, conn)
+    set_up_discogs_table(top_discogs, cur, conn)
